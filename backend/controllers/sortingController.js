d135357c72c8113f1df58f00a981ade543bbcab5
emiragovic <eagovic1@etf.unsa.ba>
2024-03-24 17:48:27 +0100

    create controller for sorting algotithms

---
+++ backend/controllers/sortingController.js
@@ -0,0 +1,91 @@
+function bubbleSort(arr) {
+  let instructions = [];
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    instructions.push({
+      description: `Starting a new pass through the array. (${i + 1} / ${n})`,
+    });
+    for (let j = 0; j < n - i - 1; j++) {
+      instructions.push({
+        type: "tag",
+        clear: true,
+        operands: [j, j + 1],
+        description: "Onto the next pair of elements.",
+      });
+      instructions.push({
+        type: "greater",
+        operands: [j, j + 1],
+        description: `Is ${arr[j]} greater than ${arr[j + 1]}?`,
+      });
+      if (arr[j] > arr[j + 1]) {
+        instructions.push({
+          type: "swap",
+          operands: [j, j + 1],
+          description: `If ${arr[j]} is greater than ${arr[j + 1]}, swap them.`,
+        });
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return instructions;
+}
+
+function selectionSort(arr) {
+  let instructions = [];
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let min_idx = i;
+    instructions.push({
+      type: "tag",
+      clear: "true",
+      operands: [i],
+      description: "New pivot element selected",
+    });
+    for (let j = i + 1; j < n; j++) {
+      instructions.push({
+        type: "tag",
+        operands: [j],
+        description: "Proceeding to next element",
+      });
+      instructions.push({
+        type: "compare",
+        operation: "less",
+        operands: [min_idx, j],
+        description: "Comparing with minimum",
+      });
+      if (arr[j] < arr[min_idx]) {
+        min_idx = j;
+        instructions.push({
+          type: "tag",
+          clear: "true",
+          operands: [min_idx, i],
+          description: "New minimum found",
+        });
+      } else {
+        instructions.push({
+          type: "tag",
+          clear: "true",
+          operands: [min_idx, i],
+          description: "No new minimum found",
+        });
+      }
+      instructions.push({
+        type: "swap",
+        operands: [min_idx, i],
+        description: "Swapping elements",
+      });
+      let temp = arr[min_idx];
+      arr[min_idx] = arr[i];
+      arr[i] = temp;
+    }
+  }
+
+  return instructions;
+}
+
+module.exports = {
+  bubbleSort,
+  selectionSort,
+};
