19a5650336ef7c946ec7434e13998b8e49f2d84f
eagovic1 <eagovic1@etf.unsa.ba>
2024-06-08 12:58:06 +0200

    implement header, edit title and icon

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <div className="company-name">
+          <h1>AlgoViz</h1>
+        </div>
+        <nav className="nav-links">
+          <Link className='nav-link' to="/">Home</Link>
+          <Link className='nav-link' to="/visualisation">Visualisation</Link>
+          <Link className='nav-link' to="/comparison">Comparison</Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
