e29b2dc269403c01d1973ca9cd810c9b8e5358a3
eagovic1 <eagovic1@etf.unsa.ba>
2024-06-09 18:07:59 +0200

    implement user controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,66 @@
+const db = require("../config/db");
+
+function register(username, password, email) {
+  const user = db.user.create({
+    username: username,
+    password: password,
+    email: email,
+  });
+  return user;
+}
+
+function editProfile(userId, username, password, email = null) {
+  const user = db.user.update(
+    {
+      username: username,
+      password: password,
+      email: email,
+    },
+    {
+      where: {
+        id: userId,
+      },
+    }
+  );
+  return user;
+}
+
+function getUserByUsername(username) {
+  const user = db.user.findOne({
+    where: {
+      username: username,
+    },
+  });
+  return user;
+}
+
+function getLogsByUserId(userId) {
+  const logs = db.log.findAll({
+    where: {
+      userId: userId,
+    },
+  });
+  return logs;
+}
+
+function login(username, password) {
+  const user = db.user.findOne({
+    where: {
+      username: username,
+      password: password,
+    },
+  });
+  return user;
+}
+
+function getRecentAlgorithms(userId) {
+  const logs = db.log.findAll({
+    where: {
+      userId: userId,
+      action: "visualization",
+    },
+    order: [["createdAt", "DESC"]],
+    limit: 5,
+  });
+  return logs;
+}
